f5df3ca492bc3f59a6ea426428ee12e5dfdfdd8c
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 01:42:39 +0800

    edited by sublime

--- gulpfile.js
+++ gulpfile.js
@@ -148,7 +148,7 @@ gulp.task('natives', function() {
         '-I<%= inc %> -c -o <%= dst(file) %>.o -g -O3',
       '<%= bin %>/pnacl-clang++ -o <%= dst(file) %>.pexe '+
         '<%= dst(file) %>.o -L<%= lib %> -lppapi_cpp -lppapi',
-      'echo `<%= nmf(dst(file)+".pexe") %>`'
+      'echo <%= nmf(dst(file)+".pexe") %>'
     ], {
       templateData: {
         bin: 'nacl_sdk/pepper_43/toolchain/mac_pnacl/bin',
@@ -159,7 +159,9 @@ gulp.task('natives', function() {
           return config.natives.destination+basename;
         },
         nmf: function(url) {
-          return JSON.stringify({program: {portable: {'pnacl-translate': {url: url}}}});
+          return JSON.stringify(
+            {program: {portable: {'pnacl-translate': {url: url}}}},
+            null, 2);
         }
       }
     }))
