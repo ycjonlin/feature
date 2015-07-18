49a70f2c2bb7ee5843d4e64187405ae3b74227ae
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 03:06:20 +0800

    edited by sublime

--- src/nacl/feature.cc
+++ src/nacl/feature.cc
@@ -166,13 +166,13 @@ class FeatureInstance : public pp::Instance {
       _interface.Set(pp::Var("matrix_determinant"), pp::Var(""));
       dictionary.Set(pp::Var("results"), _interface);
     } else if (method == "array_integral") {
-      //float* dst = static_cast<float*>(pp::VarArrayBuffer(arguments.Get(0)).Map());
-      //float* src = static_cast<float*>(pp::VarArrayBuffer(arguments.Get(1)).Map());
+      /*float* dst = static_cast<float*>(pp::VarArrayBuffer(arguments.Get(0)).Map());
+      float* src = static_cast<float*>(pp::VarArrayBuffer(arguments.Get(1)).Map());
       int i_count = arguments.Get(2).AsInt();
       int i_step  = arguments.Get(3).AsInt();
       int j_count = arguments.Get(4).AsInt();
       int j_step  = arguments.Get(5).AsInt();
-      //array_integral(dst, src, i_count, i_step, j_count, j_step);
+      //array_integral(dst, src, i_count, i_step, j_count, j_step);*/
     }
 
     PostMessage(dictionary);
