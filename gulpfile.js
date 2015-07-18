dc4cce850cc18f963f1d2c2353d13b7e312ca411
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 01:29:39 +0800

    edited by sublime

--- gulpfile.js
+++ gulpfile.js
@@ -152,7 +152,7 @@ gulp.task('natives', function() {
         inc: 'nacl_sdk/pepper_43/include',
         lib: 'nacl_sdk/pepper_43/lib/pnacl/Debug',
         dst: function(file) {
-          console.log(file);
+          console.log(file.prototype);
           return config.natives.destination+'temp';
         }
       }
