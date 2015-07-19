f327abc5dc6dde463bb0cff74cf4cd5e2777d1ab
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 16:20:19 +0800

    edited by sublime

--- src/nacl/feature.cc
+++ src/nacl/feature.cc
@@ -224,7 +224,7 @@ protected:
 
       struct stat buf;
       memset(&buf, 0, sizeof(buf));
-      stat("/mnt/local/index.html", &buf);
+      stat("/mnt/local/.", &buf);
 
       pp::VarDictionary results;
       results.Set("args", arguments);
