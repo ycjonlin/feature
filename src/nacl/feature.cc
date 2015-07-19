7dd2e45610961ef07c5ce34d969127dd913f8188
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 12:58:53 +0800

    edited by sublime

--- src/nacl/feature.cc
+++ src/nacl/feature.cc
@@ -133,6 +133,7 @@ void matrix_determinant(
 #include "ppapi/cpp/var_dictionary.h"
 #include "ppapi/cpp/url_loader.h"
 #include "ppapi/cpp/url_request_info.h"
+#include "ppapi/cpp/url_response_info.h"
 #include "ppapi/utility/completion_callback_factory.h"
 
 namespace {
