9417c1c7bb06925e9366616df08099d81b21899c
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 19:34:25 +0800

    edited by sublime

--- src/nacl/url_file.hpp
+++ src/nacl/url_file.hpp
@@ -6,30 +6,51 @@
 class URLFile {
 public:
   URLFile(const std::string &url, pp::Instance *instance)
-    : loader(instance), request(instance), callback_factory(this) {
-    request.SetURL(url);
-    request.SetMethod("GET");
-    pp::CompletionCallback callback
+    : url_loader(instance), url_request(instance), callback_factory(this)
+  {
+    url_request.SetURL(url);
+    url_request.SetMethod("GET");
+
+    pp::CompletionCallback on_open
       = callback_factory.NewCallback(&URLFile::OnOpen);
-    loader.Open(request, callback);
+    int32_t result = url_loader.Open(url_request, on_open);
+    if (PP_ERROR_WOULDBLOCK != result)
+      on_open(result);
   }
 
 protected:
-  pp::URLLoader loader;
-  pp::URLRequestInfo request;
-  pp::URLResponseInfo response;
   pp::CompletionCallbackFactory<URLFile> callback_factory;
+  pp::URLLoader url_loader;
+  pp::URLRequestInfo url_request;
+  std::vector<uint8_t> data;
 
-  void OnOpen(int32_t result) {
-    response = loader.GetResponseInfo();
-    if (response.GetStatusCode() != 200) {
+  void OnOpen(int32_t result)
+  {
+    if (result != PP_OK) {
       return;
     }
-    pp::CompletionCallback callback
-      = callback_factory.NewCallback(&URLFile::OnRead);
-    //loader.ReadResponseBody(, , callback);
+    pp::URLResponseInfo response = url_loader.GetResponseInfo();
+    if (response.is_null() || response.GetStatusCode() != 200) {
+      return;
+    }
+    Read();
   }
 
-  void OnRead(int32_t result) {
+  void OnRead(int32_t result)
+  {
+    if (result == 0) {
+      return;
+    }
+    Read();
+  }
+
+  void Read()
+  {
+    pp::CompletionCallback on_read
+      = callback_factory.NewCallback(&URLFile::OnRead);
+    int32_t result = url_loader.ReadResponseBody(
+      buffer, sizeof(buffer), on_read);
+    if (PP_ERROR_WOULDBLOCK != result)
+      on_read(result);
   }
 };
