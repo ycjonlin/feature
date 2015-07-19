8dc2ed52cf29de7c936cc7874abbfbf1036c6fa7
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 18:40:03 +0800

    edited by sublime

--- src/nacl/url_file.hpp
+++ src/nacl/url_file.hpp
@@ -1,6 +1,7 @@
 #include "ppapi/cpp/url_loader.h"
 #include "ppapi/cpp/url_request_info.h"
 #include "ppapi/cpp/url_response_info.h"
+#include "ppapi/cpp/completion_callback_factory.h"
 
 class URLFile {
 public:
