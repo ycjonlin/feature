8f07e819b4b9a46b7d596fce16085be73b0e0241
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 22:58:22 +0800

    edited by sublime

--- src/nacl/feature.cpp
+++ src/nacl/feature.cpp
@@ -246,17 +246,40 @@ protected:
     results.Set("url", url);
     results.Set("extension", extension);
     results.Set("size", (int)data.size());
+
     if (extension == ".png") {
-      //
+      PNG();
     }
     else if (extension == ".jpeg" || extension == ".jpg") {
-      //
+      JPEG();
     }
     else {
       OnDone(PP_ERROR_BADRESOURCE);
     }
+  }
 
-    OnDone(PP_OK);
+  void PNG() {
+  }
+
+  void JPEG() {
+    struct jpeg_decompress_struct cinfo;
+
+    jpeg_create_decompress(&cinfo);
+    jpeg_mem_src(&cinfo, &data[0], data.size());
+    (void) jpeg_read_header(&cinfo, TRUE);
+    (void) jpeg_start_decompress(&cinfo);
+
+    int row_stride = cinfo.output_width * cinfo.output_components;
+    std::vector<uint8_t> image(cinfo.output_height * row_stride);
+
+    while (cinfo.output_scanline < cinfo.output_height) {
+      uint8_t *buffer_array[1];
+      buffer_array[0] = &image[cinfo.output_scanline * row_stride];
+      jpeg_read_scanlines(&cinfo, buffer_array, 1);
+    }
+
+    (void) jpeg_finish_decompress(&cinfo);
+    jpeg_destroy_decompress(&cinfo);
   }
 };
 
