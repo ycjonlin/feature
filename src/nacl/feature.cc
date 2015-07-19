ed928f9b906bfcc3ac43d00ca4ae6c04baba2a9b
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 14:08:57 +0800

    edited by sublime

--- src/nacl/feature.cc
+++ src/nacl/feature.cc
@@ -205,7 +205,7 @@ protected:
       std::string library = arguments.Get(0).AsString();
       std::string path = arguments.Get(1).AsString();
       if (library == "flickr") {
-        response.Set("results", url);
+        response.Set("results", pp::Var(url));
       }
     }
     else if (method == "array_integral") {
