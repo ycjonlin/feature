f62a676e3eba0cad7b5f7d39c538d70d61597918
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 15:04:29 +0800

    edited by sublime

--- src/nacl/feature.cc
+++ src/nacl/feature.cc
@@ -214,8 +214,8 @@ protected:
 
       if (library == "flickr") {
         FILE *fp = fopen(path.str().c_str(), "rb");
-        //fseek(fp, 0L, SEEK_END);
-        response.Set("results", path.str().c_str());
+        fseek(fp, 0L, SEEK_END);
+        response.Set("results", (int32_t)ftell(fp));
       }
     }
     else if (method == "array_integral") {
