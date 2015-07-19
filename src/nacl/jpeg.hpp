367bcd7389a1477187d3ef2020e2d09d8a36250b
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-20 01:14:10 +0800

    edited by sublime

--- src/nacl/jpeg.hpp
+++ src/nacl/jpeg.hpp
@@ -15,12 +15,12 @@ my_error_exit (j_common_ptr cinfo)
   longjmp(myerr->setjmp_buffer, 1);
 }
 
-int32_t JPEG_Decode(uint8_t *data, size_t size)
+int32_t JPEG_Decode(uint8_t *data, size_t length, pp::VarDictionary &image)
 {
   struct jpeg_decompress_struct cinfo;
   struct my_error_mgr jerr;
   JSAMPARRAY buffer;
-  int row_stride;
+  int row_stride, image_size;
 
   cinfo.err = jpeg_std_error(&jerr.pub);
   jerr.pub.error_exit = my_error_exit;
@@ -30,17 +30,29 @@ int32_t JPEG_Decode(uint8_t *data, size_t size)
   }
 
   jpeg_create_decompress(&cinfo);
-  jpeg_mem_src(&cinfo, data, size);
+  jpeg_mem_src(&cinfo, data, length);
   (void) jpeg_read_header(&cinfo, TRUE);
   (void) jpeg_start_decompress(&cinfo);
   row_stride = cinfo.output_width * cinfo.output_components;
+  image_size = cinfo.output_height * row_stride;
+
+  // JS: Image Array
+  pp::VarArrayBuffer array_buffer(image_size);
+  void *array_buffer_ptr = array_buffer.Map();
+
   buffer = (*cinfo.mem->alloc_sarray)
-	((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
+    ((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
   while (cinfo.output_scanline < cinfo.output_height) {
     (void) jpeg_read_scanlines(&cinfo, buffer, 1);
   }
+
   (void) jpeg_finish_decompress(&cinfo);
   jpeg_destroy_decompress(&cinfo);
+
+  // JS: Image Object
+  image.Set("width", cinfo.output_width);
+  image.Set("height", cinfo.output_height);
+  image.Set("buffer", array_buffer);
 
   return 0;
 }
