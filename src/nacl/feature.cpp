974d9b2fbe2382d20444153ec56bfca0c79b2b2a
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 22:37:40 +0800

    edited by sublime

--- src/nacl/feature.cpp
+++ src/nacl/feature.cpp
@@ -138,8 +138,6 @@ void matrix_determinant(
 #include "ppapi/cpp/url_response_info.h"
 #include "ppapi/utility/completion_callback_factory.h"
 
-#include "url_file.hpp"
-
 namespace {
 
 }  // namespace
@@ -169,18 +167,67 @@ protected:
 class ImageImport : public Closure {
 public:
   ImageImport(std::string &id, pp::VarArray &arguments, pp::Instance *instance)
-    : Closure(id, arguments, instance), callback_factory(this) {
+    : Closure(id, arguments, instance), url_loader(instance), callback_factory(this) {
     OnCreate();
   }
 protected:
+  pp::URLLoader url_loader;
   pp::CompletionCallbackFactory<ImageImport> callback_factory;
 
   void OnCreate()
   {
     std::string url = arguments.Get(0).AsString();
-    pp::CompletionCallback on_load =
-      callback_factory.NewCallback(&ImageImport::OnLoad);
-    new URLFile(url, on_load, instance);
+    pp::URLRequestInfo url_request(instance);
+    url_request.SetURL(url);
+    url_request.SetMethod("GET");
+    url_request.SetProperty(PP_URLREQUESTPROPERTY_ALLOWCROSSORIGINREQUESTS, true);
+    url_request.SetProperty(PP_URLREQUESTPROPERTY_FOLLOWREDIRECTS, true);
+
+    pp::CompletionCallback on_open
+      = callback_factory.NewCallback(&ImageImport::OnOpen);
+    result = url_loader.Open(url_request, on_open);
+
+    if (PP_OK_COMPLETIONPENDING != result)
+      on_open.Run(result);
+  }
+
+  void OnOpen(int32_t result)
+  {
+    if (result != PP_OK) {
+      OnDone(result);
+      return;
+    }
+    pp::URLResponseInfo response = url_loader.GetResponseInfo();
+    if (response.is_null() || response.GetStatusCode() != 200) {
+      OnDone(PP_ERROR_FILENOTFOUND);
+      return;
+    }
+    Read();
+  }
+
+  void OnRead(int32_t result)
+  {
+    if (result == 0) {
+      OnLoad(result);
+      return;
+    }
+    int32_t size = std::min<int32_t>(result, sizeof(buffer));
+    data.reserve(data.size()+size);
+    data.insert(data.end(), buffer, buffer+size);
+
+    Read();
+  }
+
+  void Read()
+  {
+    pp::CompletionCallback on_read
+      = callback_factory.NewCallback(&URLFile::OnRead);
+    int32_t result = url_loader.ReadResponseBody(
+      buffer, sizeof(buffer), on_read);
+
+    if (PP_OK_COMPLETIONPENDING != result) {
+      on_read.Run(result);
+    }
   }
 
   void OnLoad(int32_t result)
