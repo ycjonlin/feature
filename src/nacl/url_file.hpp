bf658ed130ff0ab3098d2fa113e2093c6df96be2
Ying-Chen Lin <yc.jon.lin@gmail.com>
2015-07-19 19:35:09 +0800

    edited by sublime

--- src/nacl/url_file.hpp
+++ src/nacl/url_file.hpp
@@ -22,6 +22,7 @@ protected:
   pp::CompletionCallbackFactory<URLFile> callback_factory;
   pp::URLLoader url_loader;
   pp::URLRequestInfo url_request;
+  uint8_t buffer[1<<20]
   std::vector<uint8_t> data;
 
   void OnOpen(int32_t result)
