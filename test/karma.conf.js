e8e942861c3fc6d57045eaf329a2e8957bfedf73
Ying-Chen Lin <John.YC_Lin@htc.com>
2015-09-16 18:09:07 +0800

    edited by sublime

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,6 +28,13 @@ module.exports = function(config) {
     },
 
 
+    browserify: {
+      extensions: ['.coffee'],
+      transform: [],
+      debug: true,
+    },
+
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
