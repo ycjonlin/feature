d1e1a86e38f0269621b6d186b775a9e10e985c5d
Ying-Chen Lin <John.YC_Lin@htc.com>
2015-09-16 16:00:12 +0800

    edited by sublime

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       '**/*.coffee': ['coffee'],
-      '**/*.js': ['coverage']
+      '**/*.coffee': ['coverage']
     },
 
 
