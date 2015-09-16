e3461fec6bcc22d933799722dfa222f74cd2db26
Ying-Chen Lin <John.YC_Lin@htc.com>
2015-09-16 15:15:47 +0800

    edited by sublime

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['dots'],
 
 
     // web server port
@@ -63,6 +63,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   });
 };
