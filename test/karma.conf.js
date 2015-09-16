098cdbdbdda64bb1cb02eb7b9aaf8469cb340358
Ying-Chen Lin <John.YC_Lin@htc.com>
2015-09-16 16:07:18 +0800

    edited by sublime

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,10 +26,6 @@ module.exports = function(config) {
       }
     },
 
-    // list of files to exclude
-    exclude: [
-    ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
