f46a007dd627c510e54d1be33bfbfdf3e80f0c2e
Ying-Chen Lin <John.YC_Lin@htc.com>
2015-09-16 15:59:28 +0800

    edited by sublime

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -23,6 +23,11 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
+      'coverage',
+      'doc',
+      'node_modules',
+      'public',
+      'test'
     ],
 
 
