3db87ac2db0b507a7cf53ab2251efeac02e6c1f1
Ying-Chen Lin <John.YC_Lin@htc.com>
2015-09-16 17:15:13 +0800

    edited by sublime

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [],
 
+
     browserify: {
       extensions: ['.coffee'],
       transform: ['coffeeify', 'deamdify', 'debowerify'],
