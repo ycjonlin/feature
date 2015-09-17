01ec0e4b48248c3f311e5532f8e958ff8940e590
Ying-Chen Lin <John.YC_Lin@htc.com>
2015-09-17 11:37:25 +0800

    edited by sublime

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/coffee/**/*.coffee': ['coverage', 'browserify'],
+      'src/coffee/**/*.coffee': 'coverage',
       'test/unit/**/*.coffee': 'browserify'
     },
 
