c9d5c5d22a32d0df9e897f61c7286bb0a98a06e4
Ying-Chen Lin <John.YC_Lin@htc.com>
2015-09-17 15:13:47 +0800

    edited by sublime

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,9 +35,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'src/coffee/**/*.coffee': ['coffee', 'coverage', 'browserify'],
-      'test/unit/**/*.coffee': ['browserify'],
-      '**/*.js': ['coverage']
+      'src/coffee/**/*.coffee': ['browserify'],
+      'test/unit/**/*.coffee': ['browserify']
     },
 
 
@@ -60,7 +59,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress'],
 
 
     // web server port
