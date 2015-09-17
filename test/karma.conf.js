f719b57bb8011b99f2eed0eb676ea9ebf2d406f1
Ying-Chen Lin <John.YC_Lin@htc.com>
2015-09-17 12:04:12 +0800

    edited by sublime

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -52,7 +52,7 @@ module.exports = function(config) {
         ibrik: require('ibrik')
       },
       instrumenter: {
-        '**/*.coffee': 'ibrik'
+        'src/coffee/**/*.coffee': 'ibrik'
       }
     },
 
